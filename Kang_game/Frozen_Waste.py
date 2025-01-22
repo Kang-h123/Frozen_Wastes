ae61eae6a535fa5c8ace159926db2e14d4528cf2
Kang-h123 <kangh45678910@gmail.com>
2025-01-22 18:58:56 +0700

    update

--- Kang_game/Frozen_Waste.py
+++ Kang_game/Frozen_Waste.py
@@ -44,16 +44,90 @@ class Game:
             self.clock.tick(60)
     
     def starting_screen(self):
+
+
         self.background_image = pygame.image.load("Imgs/background.png")
+        self.character_image = pygame.image.load("Imgs/character.png")
+        self.character_image = pygame.transform.scale(self.character_image, (30, 80)) 
+        self.floor_image = pygame.image.load("Imgs/ground.png")
+        self.floor_image = pygame.transform.scale(self.floor_image, (200, 50))  
+
+        rect_x, rect_y = 200, 700
+        rect1_x, rect1_y = 550, 700
+        
+        self.floor_rect = self.floor_image.get_rect(topleft=(0, 750))  
+
+        self.floor_rect1 = self.floor_image.get_rect(topleft=(350, 750))
+
+        # Character properties
+        char_x, char_y = 50, 600 
+        char_velocity_x = 0
+        char_velocity_y = 0
+        gravity = 1
+        jump_power = -15
+        on_floor = False
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            self.screen.blit(self.background_image, (0,0))
+
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        char_velocity_x = 5  
+                    if event.key == pygame.K_LEFT:
+                        char_velocity_x = -5  
+                    if event.key == pygame.K_SPACE and on_floor:  
+                        char_velocity_y = jump_power
+
+                if event.type == pygame.KEYUP:
+                    if event.key in [pygame.K_RIGHT, pygame.K_LEFT]:
+                        char_velocity_x = 0  
+
+            
+            char_velocity_y += gravity
+
+            
+            char_x -= char_velocity_x
+            rect_x -= char_velocity_x
+            rect1_x -= char_velocity_x
+            char_y += char_velocity_y
+
+            char_y, char_velocity_y, a = self.check_collision(char_x, char_y, char_velocity_y, rect_x, rect_y)
+            char_y, char_velocity_y, b = self.check_collision(char_x, char_y, char_velocity_y, rect1_x, rect1_y)
+
+
+            on_floor = a or b
+
+
+            char_x = max(200, min(char_x, 650))  
+
+            # Draw background, floor, and character
+            self.screen.blit(self.background_image, (0, 0))
+            self.screen.blit(self.floor_image, (rect_x, rect_y))
+            self.screen.blit(self.floor_image, (rect1_x, rect1_y ))
+            self.screen.blit(self.character_image, (char_x, char_y))
+
             pygame.display.update()
             self.clock.tick(60)
+
+    def check_collision(self, char_x, char_y, char_velocity_y, rectx, recty):
+        on_floor = False
+        
+        if char_y + 80 > recty:  
+            if rectx <= char_x <= rectx + 200 or \
+                rectx <= char_x + 30 <= rectx + 200:
+                char_y = recty - 80 
+                char_velocity_y = 0  
+                on_floor = True
+            else:
+                on_floor = False
+        else:
+            on_floor = False
+            
+        return char_y, char_velocity_y, on_floor
 
 Game().main_menu()
 
