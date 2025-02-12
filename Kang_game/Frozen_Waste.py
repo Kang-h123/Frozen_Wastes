71d9a1899df9e5ab63e7541700398dbcff15fb7b
Kang-h123 <kangh45678910@gmail.com>
2025-02-12 18:59:25 +0700

    update 12/2

--- Kang_game/Frozen_Waste.py
+++ Kang_game/Frozen_Waste.py
@@ -32,6 +32,8 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.start_button_rect.collidepoint(event.pos):
                         self.starting_screen()
+                    elif self.tutorial_button_rect.collidepoint(event.pos):
+                        self.tutorial_screen()
                     elif self.quit_button_rect.collidepoint(event.pos):
                         pygame.quit()
                         sys.exit()
@@ -42,25 +44,60 @@ class Game:
             self.screen.blit(self.quit_button_image, self.quit_button_rect)
             pygame.display.update()
             self.clock.tick(60)
+
+    def tutorial_screen(self):
+
+        self.background_image = pygame.image.load("Imgs/tutorial_screen.png")
+        self.button_image = pygame.image.load("Imgs/tutorial_close.png")
+        self.button_image =  pygame.transform.scale(self.button_image, (300, 75))
+        self.tutorial_close_rect = self.button_image.get_rect(center = (250, 675))
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.tutorial_close_rect.collidepoint(event.pos):
+                        self.main_menu()
+
+            self.screen.blit(self.background_image, (0, 0))
+            self.screen.blit(self.button_image, self.tutorial_close_rect.center)
+            pygame.display.update()
+            self.clock.tick(60)
+
     
     def starting_screen(self):
-
 
         self.background_image = pygame.image.load("Imgs/background.png")
         self.character_image = pygame.image.load("Imgs/character.png")
         self.character_image = pygame.transform.scale(self.character_image, (30, 80)) 
         self.floor_image = pygame.image.load("Imgs/ground.png")
-        self.floor_image = pygame.transform.scale(self.floor_image, (200, 50))  
 
-        rect_x, rect_y = 200, 700
-        rect1_x, rect1_y = 550, 700
+        self.all_rect = {"rect1": [200, 700, 200, 50], "rect2": [550, 700, 400, 50], "rect3": [900, 700, 500, 50]}
+
+        for k, v in self.all_rect.items():
+            a = k + "_image"
+            setattr(self, a, pygame.transform.scale(self.floor_image, (v[-2], v[-1])))
+            setattr(self, k, self.floor_image.get_rect(topleft=(v[0], v[1])))
         
-        self.floor_rect = self.floor_image.get_rect(topleft=(0, 750))  
+        # self.floor_image = pygame.transform.scale(self.floor_image, (200, 50))  
 
-        self.floor_rect1 = self.floor_image.get_rect(topleft=(350, 750))
+        # rect_x, rect_y = 200, 700
+        # rect1_x, rect1_y = 550, 700
+        # rect2_x, rect2_y = 800, 700
+        # rect3_x, rect3_y = 1000, 700
+        
+        # self.floor_rect = self.floor_image.get_rect(topleft=(0, 750))  
+
+        # self.floor_rect1 = self.floor_image.get_rect(topleft=(350, 750))
+
+        # self.floor_rect2 =self.floor_image.get_rect(topleft=(700, 750))
+
+        # self.floor_rect3 =self.floor_image.get_rect(topleft=(1050, 750))
 
         # Character properties
-        char_x, char_y = 50, 600 
+        char_x, char_y = 50, 400 
         char_velocity_x = 0
         char_velocity_y = 0
         gravity = 1
@@ -91,42 +128,49 @@ class Game:
 
             
             char_x -= char_velocity_x
-            rect_x -= char_velocity_x
-            rect1_x -= char_velocity_x
+            for k, v in self.all_rect.items():
+                v[0] -= char_velocity_x 
+
             char_y += char_velocity_y
 
-            char_y, char_velocity_y, a = self.check_collision(char_x, char_y, char_velocity_y, rect_x, rect_y)
-            char_y, char_velocity_y, b = self.check_collision(char_x, char_y, char_velocity_y, rect1_x, rect1_y)
 
 
-            on_floor = a or b
+            on_floor = False
+            for k, v in self.all_rect.items():
+                char_y, char_velocity_y, a = self.check_collision(char_x, char_y, char_velocity_y, v[0],v[1], v[2],v[3])
+                on_floor = on_floor or a
 
 
             char_x = max(200, min(char_x, 650))  
 
             # Draw background, floor, and character
             self.screen.blit(self.background_image, (0, 0))
-            self.screen.blit(self.floor_image, (rect_x, rect_y))
-            self.screen.blit(self.floor_image, (rect1_x, rect1_y ))
+            for k, v in self.all_rect.items():
+                a = k + "_image"
+                self.screen.blit(getattr(self, a), (v[0], v[1]))
             self.screen.blit(self.character_image, (char_x, char_y))
 
             pygame.display.update()
             self.clock.tick(60)
 
-    def check_collision(self, char_x, char_y, char_velocity_y, rectx, recty):
+    def check_collision(self, char_x, char_y, char_velocity_y, rectx, recty, sizex, sizey):
         on_floor = False
         
-        if char_y + 80 > recty:  
-            if rectx <= char_x <= rectx + 200 or \
-                rectx <= char_x + 30 <= rectx + 200:
-                char_y = recty - 80 
-                char_velocity_y = 0  
+        if recty < char_y + 80 < recty + 20:
+            if rectx <= char_x <= rectx + sizex or \
+               rectx <= char_x + 30 <= rectx + sizex:
+                char_y = recty - 80
+                char_velocity_y = 0
                 on_floor = True
             else:
                 on_floor = False
-        else:
+        elif recty + 20 < char_y + 80 < recty + sizey:
+            if rectx < char_x + 30 < rectx + (sizex / 2):
+                char_x  = rectx - 30
+            else:
+                char_x = rectx + sizex
             on_floor = False
-            
+        
         return char_y, char_velocity_y, on_floor
 
 Game().main_menu()
