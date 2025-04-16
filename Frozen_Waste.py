c85f5e06c3a15b2f745cb55ee3d2d50353aea4be
Kang-h123 <kangh45678910@gmail.com>
2025-04-16 18:44:17 +0700

    update 16/4/25

--- Frozen_Waste.py
+++ Frozen_Waste.py
@@ -67,10 +67,175 @@ class Game:
             pygame.display.update()
             self.clock.tick(60)
 
-    
+    def level_2_screen(self):
+        enemy_size_x = 30
+        enemy_size_y = 50
+        spire_x = 3350
+        self.spire_image = pygame.image.load("Imgs/level2_exit.png")
+        self.spire_image = pygame.transform.scale(self.spire_image, (250, 600))
+
+        self.all_rect = {"rect1": [300, 700, 200, 50], "rect2": [575, 600, 350, 150], 
+                         "rect3": [1050, 600, 600, 150], "rect4": [1700, 500, 600, 150], 
+                         "rect5": [2300, 500, 350, 350], "rect6": [2650, 600, 250, 250],
+                         "rect7": [3050, 700, 550, 50]}
+        self.all_enemy = {
+            "enemy1": [1350, 525], "enemy2": [2000, 425], "enemy3": [2340, 325], "enemy4": [2450, 425], "enemy5": [2800, 525], "enemy6": [1650, 425], "enemy7":[2250, 425]
+        }
+
+        for k, v in self.all_enemy.items():
+            setattr(self, k, self.enemy_image.get_rect(topleft = (v[0], v[1])))
+
+        for k, v in self.all_rect.items():
+            a = k + "_image"
+            setattr(self, a, pygame.transform.scale(self.floor_image, (v[-2], v[-1])))
+            setattr(self, k, self.floor_image.get_rect(topleft=(v[0], v[1])))
+        
+
+
+        # Character properties
+        char_x, char_y = 50, 400 
+        char_velocity_x = 0
+        char_velocity_y = 0
+        gravity = 1
+        jump_power = -15
+        on_floor = False
+        
+        health = 3
+        self.start = time.time()
+
+        self.direction = 'right'
+        slash = False
+        startslash = 0
+        endslash = 0
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        if slash != True:
+                            char_velocity_x = 5  
+                            self.direction = 'right'
+                    if event.key == pygame.K_LEFT:
+                        if slash != True:
+                            char_velocity_x = -5  
+                            self.direction = 'left'
+                    if event.key == pygame.K_SPACE and on_floor:  
+                        char_velocity_y = jump_power
+                    if event.key == pygame.K_c:
+                        self.all_enemy = self.slashed(char_x, char_y, self.direction, self.all_enemy, enemy_size_x, enemy_size_y)
+                        if slash == True:
+                            pass
+                        else:
+                            slash = True
+                            startslash = time.time()
+                    if event.key == pygame.K_e:
+                        next_level = self.next_level(char_x, spire_x, 250)
+                        if next_level:
+                            self.GameWin()
+                        
+
+                if event.type == pygame.KEYUP:
+                    if event.key in [pygame.K_RIGHT, pygame.K_LEFT]:
+                        char_velocity_x = 0  
+
+            
+            char_velocity_y += gravity
+
+            
+            if slash == True:
+                char_velocity_x = 0
+
+            char_x -= char_velocity_x
+            spire_x -= char_velocity_x
+            for k, v in self.all_rect.items():
+                v[0] -= char_velocity_x 
+            for k, v in self.all_enemy.items():
+                v[0] -= char_velocity_x
+            char_y += char_velocity_y
+
+
+
+            on_floor = False
+            for k, v in self.all_rect.items():
+                char_y, char_velocity_y, a = self.check_collision(char_x, char_y, char_velocity_y, v[0],v[1], v[2],v[3])
+                on_floor = on_floor or a
+
+
+            char_x = max(300, 300)  
+
+
+            if self.touched_enemy(char_x, char_y, self.all_enemy, enemy_size_x, enemy_size_y):
+                self.end = time.time()
+                if (self.end - self.start) > 0.8:
+                    health -= 1
+                    self.start = time.time()
+                
+                
+
+            if health == 3:
+                self.health_image = pygame.image.load("Imgs/full_health.png")
+            elif health == 2:
+                self.health_image = pygame.image.load("Imgs/half_health.png")
+            elif health == 1:
+                self.health_image = pygame.image.load("Imgs/low_health.png")
+            else:
+                self.GameOver()
+
+            if self.fall(char_y):
+                self.GameOver()
+            
+             
+
+        
+            
+            
+
+            
+            
+            
+            # Draw background, floor, and character
+            self.screen.blit(self.background_image, (0, 0))
+            for k, v in self.all_rect.items():
+                a = k + "_image"
+                self.screen.blit(getattr(self, a), (v[0], v[1]))
+            for k, v in self.all_enemy.items():
+                self.screen.blit(self.enemy_image, (v[0], v[1]))
+            self.screen.blit(self.spire_image, (spire_x, 100))
+            if self.direction == 'right':
+                self.screen.blit(self.character_image, (char_x, char_y))
+            else:
+                self.screen.blit(pygame.transform.flip(self.character_image, True, False), (char_x, char_y))
+            
+            self.screen.blit(self.health_image, (20, 50))
+            
+
+            if slash == True:
+                endslash = time.time()
+                
+                if (endslash - startslash) < 0.8:                  
+                    if self.direction == 'left':
+                        slashx = char_x - 40
+                        slashy = char_y + 20
+                    else:
+                        slashx = char_x + 30
+                        slashy = char_y + 20
+                    if self.direction == 'right':
+                        self.screen.blit(self.sword_slash, (slashx, slashy))
+                    else:
+                        self.screen.blit(pygame.transform.flip(self.sword_slash, True, False), (slashx, slashy))
+                else:
+                    slash = False
+
+            pygame.display.update()
+            self.clock.tick(60)
+        
     def starting_screen(self):
 
-        self.background_image = pygame.image.load("Imgs/background.png")
+        self.background_image = pygame.image.load("Imgs/forest.png")
         self.character_image = pygame.image.load("Imgs/character.png")
         self.character_image = pygame.transform.scale(self.character_image, (30, 80)) 
         self.floor_image = pygame.image.load("Imgs/ground.png")
@@ -78,10 +243,18 @@ class Game:
         self.health_image = pygame.image.load("Imgs/full_health.png")
         self.sword_slash = pygame.image.load("Imgs/sword_slash.png")
         self.sword_slash = pygame.transform.scale(self.sword_slash, (40, 40))
+        self.spire_image = pygame.image.load("Imgs/lonely_spire.png")
+        self.spire_image = pygame.transform.scale(self.spire_image, (250, 600))
+        self.spire_rect = self.spire_image.get_rect(topleft = (3350, 100))
+        
         
 
         enemy_size_x = 30
         enemy_size_y = 50
+        spire_x = 3350
+        
+
+
         self.enemy_image = pygame.transform.scale(self.enemy_image, (enemy_size_x, enemy_size_y))
 
         self.all_rect = {"rect1": [300, 700, 200, 50], "rect2": [575, 700, 350, 50], 
@@ -142,6 +315,11 @@ class Game:
                         else:
                             slash = True
                             startslash = time.time()
+                    if event.key == pygame.K_e:
+                        next_level = self.next_level(char_x, spire_x, 250)
+                        if next_level:
+                            self.level_2_screen()
+                        
                         
                         
 
@@ -158,6 +336,7 @@ class Game:
                 char_velocity_x = 0
 
             char_x -= char_velocity_x
+            spire_x -= char_velocity_x
             for k, v in self.all_rect.items():
                 v[0] -= char_velocity_x 
             for k, v in self.all_enemy.items():
@@ -211,12 +390,14 @@ class Game:
                 self.screen.blit(getattr(self, a), (v[0], v[1]))
             for k, v in self.all_enemy.items():
                 self.screen.blit(self.enemy_image, (v[0], v[1]))
+            self.screen.blit(self.spire_image, (spire_x, 100))
             if self.direction == 'right':
                 self.screen.blit(self.character_image, (char_x, char_y))
             else:
                 self.screen.blit(pygame.transform.flip(self.character_image, True, False), (char_x, char_y))
             
             self.screen.blit(self.health_image, (20, 50))
+            
 
             if slash == True:
                 endslash = time.time()
@@ -256,14 +437,23 @@ class Game:
             
             self.screen.blit(self.background_image, (0, 0))
             self.screen.blit(self.button_image, self.tryagain_button_rect.center)
+
+
+
             pygame.display.update()
             self.clock.tick(60)
 
-
-
+    def GameWin(self):
+        self.background_image = pygame.image.load("Imgs/win_screen.png")
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                        
             
-
-
+            self.screen.blit(self.background_image, (0, 0))
 
             pygame.display.update()
             self.clock.tick(60)
@@ -321,6 +511,13 @@ class Game:
                     break
         
         return enemy_list
+
+    def next_level(self, char_x, towerx, tsizex):
+        touched = False
+        if towerx < char_x < towerx + tsizex or \
+            towerx + tsizex > char_x + 30 > towerx:
+            touched = True
+        return touched
 
     def fall(self, char_y):
         if char_y > 780:
